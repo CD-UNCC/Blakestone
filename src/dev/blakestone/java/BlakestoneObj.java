1edf8cde96cde63f987470fd0a08993883a7a70e
Austin Beeler <abeeler1@uncc.edu>
2015-06-05 18:10:06 -0400

    Object properly parsed and stored. Generated values not yet implemented

--- src/dev/blakestone/java/BlakestoneObj.java
+++ src/dev/blakestone/java/BlakestoneObj.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,15 +35,67 @@ public class BlakestoneObj {
 	private final static Pattern INDEX_VALUE_PATTERN = Pattern.compile(":\\s*(.+)");
 	
 	private HashMap<String, Object> keyVals;
-	private ArrayList<Object>[] arr;
+	private ArrayList<Object> arr;
 	
-	public BlakestoneObj(InputStream in) throws IOException {
+	public BlakestoneObj() {
+		keyVals = new HashMap<>();
+		arr = new ArrayList<>();
+	}
+	
+	public void add(Object obj) {
+		arr.add(obj);
+	}
+	
+	public void add(String key, Object value) {
+		keyVals.put(key, value);
+	}
+	
+	public Object get(String key) {
+		return keyVals.get(key);
+	}
+	
+	public Object get(int index) {
+		return arr.get(index);
+	}
+	
+	public int size() { return arr.size(); }
+	
+	@Override
+	public String toString() {
+		String result = "";
+		
+		for (int i = 0; i < arr.size(); i++)
+			if (arr.get(i) instanceof BlakestoneObj) {
+				result += i + ": \n";
+				for (String line : arr.get(i).toString().split("\\r?\\n"))
+					result += "   " + line + "\n";
+			} else
+				result += i + ": " + arr.get(i).toString() + "\n";
+				
+		ArrayList<String> sortedKeys = new ArrayList<>(keyVals.keySet());
+		Collections.sort(sortedKeys);
+		
+		for (String key : sortedKeys)
+			if (keyVals.get(key) instanceof BlakestoneObj) {
+				result += key + ": \n";
+				for (String line : keyVals.get(key).toString().split("\\r?\\n"))
+					result += "   " + line + "\n";
+			} else
+				result += key + ": " + keyVals.get(key).toString() + "\n";
+		
+		return result;
+	}
+	
+	public static BlakestoneObj read(InputStream in) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 		String line;
 		String indent = null;
 		Matcher m;
 		
+		int level, lastLevel = 0;
 		
+		Stack<BlakestoneObj> currentParent = new Stack<>();
+		currentParent.push(new BlakestoneObj());
 		
 		while ((line = reader.readLine()) != null) {
 			if (indent == null) {
@@ -52,12 +106,18 @@ public class BlakestoneObj {
 			
 			String type = "value";
 			
-			int level = 0;
+			level = 0;
 			if (indent != null)
 				while (line.startsWith(indent)) {
 					level++;
 					line = line.substring(indent.length());
 				}
+			
+			
+			if (level < lastLevel)
+				currentParent.pop();
+				
+			lastLevel = level;
 			
 			m = COMMENT_PATTERN.matcher(line);
 			if (m.find())
@@ -65,9 +125,9 @@ public class BlakestoneObj {
 			
 			line = line.trim();
 			
-			if (KEY_PATTERN.matcher(line).matches())
+			if ((m = KEY_PATTERN.matcher(line)).matches())
 				type = "key";
-			else if (INDEX_PATTERN.matcher(line).matches())
+			else if ((m = INDEX_PATTERN.matcher(line)).matches())
 				type = "index";
 			else if ((m = INDEX_VALUE_PATTERN.matcher(line)).matches())
 				type = "index-value";
@@ -76,29 +136,52 @@ public class BlakestoneObj {
 			
 			if (type.endsWith("value")) {
 				String value;
+				String key = null;
 				
-				if (type.equals("key-value"))
+				if (type.equals("key-value")) {
 					value = m.group(2);
+					key = m.group(1);
+				}
 				else if (type.equals("index-value"))
 					value = m.group(1);
-				else
+				else {
+					type = "index-value";
 					value = line;
+				}					
 				
 				m = VALUE_PATTERN.matcher(value);				
 				
 				if (m.find()) {
+					Object obj = null;
+					
 					if (STRING_PATTERN.matcher(value).matches())
-						type += "-str";
+						obj = value.substring(1, value.length() - 1);
 					else if (INTEGER_PATTERN.matcher(value).matches())
-						type += "-int";
+						obj = value.startsWith("%") ? -1 : Integer.parseInt(value);
 					else if (FLOATING_POINT_PATTERN.matcher(value).matches())
-						type += "-flt";
+						obj = value.startsWith("%") ? -0.1 : Float.parseFloat(value);
+					
+					if (type.startsWith("index"))
+						currentParent.peek().add(obj);
+					else
+						currentParent.peek().add(key, obj);
 				}
-			}		
-				
-			System.out.printf("%2d (%S) | %s%n", level, type, line);
+			} else {
+				BlakestoneObj newTop = new BlakestoneObj();
+				if (type.equals("key")) 
+					currentParent.peek().add(m.group(1), newTop);
+				else
+					currentParent.peek().add(newTop);
+			
+				currentParent.push(newTop);
+			}
 		}
+		
+		while (currentParent.size() > 1)
+			currentParent.pop();
+		
+		return currentParent.pop();
 	}
 	
-	public BlakestoneObj(File f) throws FileNotFoundException, IOException { this(new FileInputStream(f)); }
+	public static BlakestoneObj read(File f) throws IOException, FileNotFoundException { return read(new FileInputStream(f)); }
 }
--- src/dev/blakestone/java/ParseTest.java
+++ src/dev/blakestone/java/ParseTest.java
@@ -7,8 +7,12 @@ public class ParseTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			BlakestoneObj obj = new BlakestoneObj(new File("blakestone.bs"));
-		} catch (Exception e) { }
+			BlakestoneObj obj = BlakestoneObj.read(new File("blakestone.bs"));
+			System.out.println(obj);
+			System.out.println("-");
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
 	}
 
 }
